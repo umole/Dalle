7ca01136708e2694c9e54c0ed10c0f4bdf735bbd
Umole Andrew <39883080+umole@users.noreply.github.com>
2023-02-12 17:25:29 +0100

    updating coponents

---
+++ client/Dalle/src/utils/index.js
@@ -0,0 +1,11 @@
+import FileSaver from 'file-saver';
+import { surpriseMePrompts } from '../constants';
+
+export function getRandomPrompt(prompt) {
+    const randomIndex = math.floor(math.random() * surpriseMePrompts.length);
+    const randomPrompt = surpriseMePrompts[randomIndex];
+
+    if (randomPrompt === prompt) return getRandomPrompt(prompt);
+
+    return randomPrompt;
+}
