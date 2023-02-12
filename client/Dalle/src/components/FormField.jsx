b49bbe4b18de3ddade56ad9ad955fa16f96bdf1e
Umole Andrew <39883080+umole@users.noreply.github.com>
2023-02-12 17:28:39 +0100

    Updated the FormField component

--- client/Dalle/src/components/FormField.jsx
+++ client/Dalle/src/components/FormField.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-const FormField = (labelName, type, name, placeholder, value, handleChange, isSurpriseMe, handleSurpriseMe) => {
+const FormField = ([labelName, type, name, placeholder, value, handleChange, isSurpriseMe, handleSurpriseMe]) => {
   return (
     <div>
       <div className='flex items-center gap-2 mb-2'>
@@ -12,11 +12,11 @@ const FormField = (labelName, type, name, placeholder, value, handleChange, isSu
         </label>
         {isSurpriseMe && (
           <button 
-              type='button'
-              onClick={handleSurpriseMe}
-              className="font-semibold text-xs bg-[#EcECF1] py-1 px-2 rounded-[5px] text-black"
-              >
-
+            type='button'
+            onClick={handleSurpriseMe}
+            className="font-semibold text-xs bg-[#EcECF1] py-1 px-2 rounded-[5px] text-black"
+          >
+              Surprise me
           </button>
         )}
       </div>
