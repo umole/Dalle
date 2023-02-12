3a515b75b923efdb4d65e883b310dd088e2ff2ff
Umole Andrew <39883080+umole@users.noreply.github.com>
2023-02-12 15:25:32 +0100

    Adding some components

--- client/Dalle/tailwind.config.cjs
+++ client/Dalle/tailwind.config.cjs
@@ -18,3 +18,16 @@ module.exports = {
   },
   plugins: [],
 };
+/** 
+ * 
+// @type {import('tailwindcss').Config} */
+// module.exports = {
+//   content: [
+//     "./index.html",
+//     "./src/**/*.{js,ts,jsx,tsx}",
+//   ],
+//   theme: {
+//     extend: {},
+//   },
+//   plugins: [],
+// }
