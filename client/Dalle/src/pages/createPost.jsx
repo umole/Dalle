cd0c8fcf2a1ad9f920a305440b37af6eb5fdfbaa
Umole Andrew <39883080+umole@users.noreply.github.com>
2023-02-13 23:07:07 +0100

    update backend

--- client/Dalle/src/pages/createPost.jsx
+++ client/Dalle/src/pages/createPost.jsx
@@ -17,8 +17,27 @@ const createPost = () => {
   const [loading, setLoading] = useState(false);
 
 
-  function generateImage() {
-
+  async function generateImage() {
+    if (form.prompt) {
+      try {
+        setGeneratingImg(true);
+        const response = await fetch('http://localhost:3000/api/v1/dalle', {
+          method: 'POST',
+          headers: {
+            'content-Type': 'application/json',
+          },
+          body: JSON.stringify({prompt: form.prompt})
+        }) 
+        const data = await response.json();
+        setForm({...form, photo: `data:image/jpeg;base64,${data.photo}`})
+      } catch (error) {
+        console.error(error);
+      } finally {
+        setGeneratingImg(false);
+      }
+    } else {
+      alert('Please enter a prompt');
+    }
   }
 
   function handleChange(event) {
